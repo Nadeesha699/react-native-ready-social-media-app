1e4d7aa8bc4b4dc8c8f593340fe8c64b6fb7c03a
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2024-11-27 15:43:22 +0530

    saved

---
+++ app/Home.tsx
@@ -0,0 +1,16 @@
+
+
+import { View, Text } from "react-native";
+
+const Home = () =>{
+    return(
+        <View>
+           <Text>Hello world</Text>
+           <Text>Nadeesha</Text>
+            <Text>oyata jeewithaya jayaganna onada</Text>
+            <Text>hari halo</Text>
+        </View>
+    )
+}
+
+export default Home;
