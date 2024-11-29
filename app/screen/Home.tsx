82d6eab41147acf928a06804ef44399fdbbf320c
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2024-11-29 14:12:56 +0530

    saved

--- app/screen/Home.tsx
+++ app/screen/Home.tsx
@@ -1,13 +1,18 @@
-import { StatusBars } from '@/components/components'
-import React from 'react'
-import { View,Text, StatusBar } from 'react-native'
+import { StatusBars } from "@/components/components";
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
 
 function Home() {
   return (
-    <><StatusBars/><View>
-      <Text>හලෝ මගේ රත්තරන් යාලුවනේ කොහොමද හොදින් ඉන්නවද </Text>
-    </View></>
-  )
+    <>
+      <StatusBars />
+      <View>
+        <Text style={{ fontSize: 60 }}>
+          හලෝ මගේ රත්තරන් යාලුවනේ කොහොමද හොදින් ඉන්නවද{" "}
+        </Text>
+      </View>
+    </>
+  );
 }
 
-export default Home
+export default Home;
